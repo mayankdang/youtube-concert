744a4378df4c1f1f009c60048f1c08eaf2d9a223
Rishabh Jain <dce.rishabh@gmail.com>
2015-06-04 21:57:33 +0530

    Intermidiate solution.

--- simpleWebSocketServer/SimpleExampleServer.py
+++ simpleWebSocketServer/SimpleExampleServer.py
@@ -1,135 +1,74 @@
-'''
-The MIT License (MIT)
-Copyright (c) 2013 Dave P.
-'''
-
+import json
 import signal, sys, ssl
-import json, time, string, random
 from SimpleWebSocketServer import WebSocket, SimpleWebSocketServer, SimpleSSLWebSocketServer
 from optparse import OptionParser
 
-PORT = 8000
-IP = "localhost"
 
 class SimpleEcho(WebSocket):
 
-    def handleMessage(self):
-        self.sendMessage(self.data)
+   def handleMessage(self):
+      self.sendMessage(self.data)
 
-    def handleConnected(self):
-        pass
+   def handleConnected(self):
+      pass
 
-    def handleClose(self):
-        pass
+   def handleClose(self):
+      pass
 
 clients = []
-userIdMainMap = {}
 
-# class Group(object):
-#     def __init__(self):
-#         self.groupId = self.id_generator(10)
-#         self.users = {}
-#         self.maxNetworkDelayOfUsers = 100
-#
-#     def setMaxNetworkDelayOfUsers(self):
-#         maxDelay = 0
-#         for key, value in self.users.iteritems():
-#             maxDelay = max(maxDelay, value.networkDelay)
-#         self.maxNetworkDelayOfUsers = maxDelay
-#
-#     def id_generator(self, size=6, chars=string.ascii_uppercase + string.digits):
-#         return ''.join(random.choice(chars) for _ in xrange(size))
-
-# class User(object):
-#     def __init__(self, userId, client):
-#         self.userId = userId
-#         self.networkDelay = 15      # default
-#         self.recentTime = -1
-#         self.concertId = 0
-#         self.client = client
-#     def getCurrentTime(self):
-#         return int(round(time.time() * 1000))
-#     def updateRecentTime(self):
-#         self.recentTime = self.getCurrentTime()
-#     def setNetworkDelay(self, delay):
-#         self.networkDelay = delay
-#         print "set network delay as", delay
 
 class SimpleChat(WebSocket):
 
-    def handleMessage(self):
-        try:
-            userMessageIdMap = json.loads(self.data)
-            userId = str(userMessageIdMap.get("id"))
-            message = str(userMessageIdMap.get("message"))
-            print "userId:", userId
-            print "message:", message
+   def handleMessage(self): 
+      message=json.loads(self.data)
+      self.sendMessage(self.address[0] + ' - ' + self.data)
+      msg= message["message"]
+      
+      if msg.startswith("HELLO_BUDDY"):
+		   self.sendMessage('HEY_BUDDY:'+msg.split(":")[1])            
 
-            if message.startswith("HELLO_BUDDY"):
-                self.sendMessage("HEY_BUDDY:" + message.split(':')[1])
-            elif message.startswith("NETWORK_DELAY"):
-                delay = int(message.split(':')[1])
-                self.sendMessage("******************************HEY_NETWORK!:" + str(delay))
-                print "delay:", delay
-                # if userIdMainMap.get(userId, -1) == (-1):
-                #     userIdMainMap[userId] = User(userId, self)
-                #     print "putting in map"
-                # userIdMainMap[userId].setNetworkDelay(delay)
+      if msg.startswith("NETWORK_DELAY"):
+		   self.sendMessage('HANDSHAKING_DONE:'+msg.split(":")[1])            
 
-                # print userIdMainMap[userId].userId
-                # print userIdMainMap[userId].client.address
+   def handleConnected(self):
+      print self.address, 'connected'
+      for client in list(clients):
+         client.sendMessage(self.address[0] + u' - connected')
+      clients.append(self)
 
-                self.sendMessage('HeyYo! to self')
-                # clients[0].sendMessage("array wala send message")
-                # print type(userIdMainMap[userId].client)
-                print type(self)
-
-            else:
-                for client in list(clients):
-                    if client != self:
-                        client.sendMessage(self.address[0] + ' - ' + message)
-
-        except Exception, e:
-            print "Exception." + e
-
-    def handleConnected(self):
-        print self.address, 'connected'
-        for client in list(clients):
-            client.sendMessage(self.address[0] + u' - connected')
-        clients.append(self)
-
-    def handleClose(self):
-        clients.remove(self)
-        print self.address, 'closed'
-        for client in list(clients):
-            client.sendMessage(self.address[0] + u' - disconnected')
-
+   def handleClose(self):
+      clients.remove(self)
+      print self.address, 'closed'
+      for client in list(clients):
+         client.sendMessage(self.address[0] + u' - disconnected')
+      
 
 if __name__ == "__main__":
 
-    parser = OptionParser(usage="usage: %prog [options]", version="%prog 1.0")
-    parser.add_option("--host", default='', type='string', action="store", dest="host", help="hostname (localhost)")
-    parser.add_option("--port", default=PORT, type='int', action="store", dest="port", help="port (" + str(PORT) + ")")
-    parser.add_option("--example", default='echo', type='string', action="store", dest="example", help="echo, chat")
-    parser.add_option("--ssl", default=0, type='int', action="store", dest="ssl", help="ssl (1: on, 0: off (default))")
-    parser.add_option("--cert", default='./cert.pem', type='string', action="store", dest="cert", help="cert (./cert.pem)")
-    parser.add_option("--ver", default=ssl.PROTOCOL_TLSv1, type=int, action="store", dest="ver", help="ssl version")
+   parser = OptionParser(usage="usage: %prog [options]", version="%prog 1.0")
+   parser.add_option("--host", default='', type='string', action="store", dest="host", help="hostname (localhost)")
+   parser.add_option("--port", default=8000, type='int', action="store", dest="port", help="port (8000)")
+   parser.add_option("--example", default='echo', type='string', action="store", dest="example", help="echo, chat")
+   parser.add_option("--ssl", default=0, type='int', action="store", dest="ssl", help="ssl (1: on, 0: off (default))")
+   parser.add_option("--cert", default='./cert.pem', type='string', action="store", dest="cert", help="cert (./cert.pem)")
+   parser.add_option("--ver", default=ssl.PROTOCOL_TLSv1, type=int, action="store", dest="ver", help="ssl version")
+   
+   (options, args) = parser.parse_args()
 
-    (options, args) = parser.parse_args()
+   cls = SimpleEcho
+   if options.example == 'chat':
+      cls = SimpleChat	
 
-    cls = SimpleEcho
-    if options.example == 'chat':
-        cls = SimpleChat
+   if options.ssl == 1:
+      server = SimpleSSLWebSocketServer(options.host, options.port, cls, options.cert, options.cert, version=options.ver)
+   else:	
+      server = SimpleWebSocketServer(options.host, options.port, cls)
 
-    if options.ssl == 1:
-        server = SimpleSSLWebSocketServer(options.host, options.port, cls, options.cert, options.cert, version=options.ver)
-    else:
-        server = SimpleWebSocketServer(options.host, options.port, cls)
+   def close_sig_handler(signal, frame):
+      server.close()
+      sys.exit()
 
-    def close_sig_handler(signal, frame):
-        server.close()
-        sys.exit()
+   signal.signal(signal.SIGINT, close_sig_handler)
 
-    signal.signal(signal.SIGINT, close_sig_handler)
-
-    server.serveforever()
+   server.serveforever()
