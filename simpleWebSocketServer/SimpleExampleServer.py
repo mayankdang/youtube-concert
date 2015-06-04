0c8cd6288f5c0496fe9b039ec93c5902636549d6
VARUN JUNEJA <varunjuneja05@gmail.com>
2015-06-04 23:51:40 +0530

    User class complete

--- simpleWebSocketServer/SimpleExampleServer.py
+++ simpleWebSocketServer/SimpleExampleServer.py
@@ -2,6 +2,7 @@ import json
 import signal, sys, ssl
 from SimpleWebSocketServer import WebSocket, SimpleWebSocketServer, SimpleSSLWebSocketServer
 from optparse import OptionParser
+import string
 
 
 class SimpleEcho(WebSocket):
@@ -17,6 +18,37 @@ class SimpleEcho(WebSocket):
 
 clients = []
 
+userIdMainMap = {}
+
+class Group(object):
+    def __init__(self):
+        self.groupId = self.id_generator(10)
+        self.users = {}
+        self.maxNetworkDelayOfUsers = 100
+
+    def setMaxNetworkDelayOfUsers(self):
+        maxDelay = 0
+        for key, value in self.users.iteritems():
+            maxDelay = max(maxDelay, value.networkDelay)
+        self.maxNetworkDelayOfUsers = maxDelay
+
+    def id_generator(self, size=6, chars=string.ascii_uppercase + string.digits):
+        return ''.join(random.choice(chars) for _ in xrange(size))
+
+class User(object):
+    def __init__(self, userId, client):
+        self.userId = userId
+        self.networkDelay = 15      # default
+        self.recentTime = -1
+        self.concertId = 0
+        self.client = client
+    def getCurrentTime(self):
+        return int(round(time.time() * 1000))
+    def updateRecentTime(self):
+        self.recentTime = self.getCurrentTime()
+    def setNetworkDelay(self, delay):
+        self.networkDelay = delay
+        print "set network delay as", delay
 
 class SimpleChat(WebSocket):
 
@@ -32,6 +64,16 @@ class SimpleChat(WebSocket):
             delay = msg.split(":")[1]
             print "Handshaking done", delay, "Sending confirmation to client."
             self.sendMessage("HANDSHAKING_DONE:"+msg.split(":")[1])
+            if userIdMainMap.get(userId, -1) == (-1):
+                userIdMainMap[userId] = User(userId, self)
+                # print "putting in map"
+            userIdMainMap[userId].setNetworkDelay(delay)
+            # userIdMainMap[userId].client.sendMessage(u'client wala send message')
+
+        else:
+            for client in list(clients):
+                if client != self:
+                    client.sendMessage(self.address[0] + ' - ' + message)
 
     def handleConnected(self):
         print self.address, 'connected'
