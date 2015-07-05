307a4216187d9de873fb08ea0196c543db4c666e
Mayank Dang <mayank.d@directi.com>
2015-07-05 18:35:09 +0530

    Changing ip

--- restartClient.sh
+++ restartClient.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 (cd $1; git reset --hard)
-(cd $1; git pull origin live)
-(cd $1; git checkout origin/live)
+(cd $1; git pull origin stage)
+(cd $1; git checkout origin/stage)
 (cd $1;sudo python kango-framework-latest/kango.py build extension)
